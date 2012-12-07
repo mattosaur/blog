b652d2e8d1bbe7a92d19a43bd73bee3b0e1ff269
Matthew Dyer <matthew.b.dyer@gmail.com>
2012-12-07 17:13:59 -0500

    Initial checkin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f8147c9b0f84786a2f5dfb5ebf174816f67e2232d48a80ee6c510ce927196eef3eeb740e83fde1e21c6801419f9fbd7443306fc8929ea3c6838abb7d9458809b'
